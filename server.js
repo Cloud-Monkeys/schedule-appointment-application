86a268e47669b9b64f02f0600a41aaa09551cc2e
nuocwang <wang.nuoc@northeastern.edu>
2024-10-19 19:52:14 -0400

    remove course membership

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 expressOasGenerator.init(app);
 const scheduleService = require('./services/scheduleService');
 const appointmentService = require('./services/appointmentService');
-// const courseMembershipService = require('./services/courseMembershipService');
 
 app.use(express.json());
 app.use(cors());
@@ -14,7 +13,6 @@ app.use(cors());
 // Define specific base routes for each service
 app.use('/schedules', scheduleService);
 app.use('/appointments', appointmentService);
-// app.use('/course-memberships', courseMembershipService);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
