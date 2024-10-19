ccf22d9444cc5be9a2ec39e3b9e2ccfb046923dc
nuocwang <wang.nuoc@northeastern.edu>
2024-10-19 14:53:28 -0400

    add OpenAPI documentation
    add OpenAPI documentation
    remove course services

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const expressOasGenerator = require('express-oas-generator');
 const cors = require('cors');
 const app = express();
+expressOasGenerator.init(app);
 const scheduleService = require('./services/scheduleService');
 const appointmentService = require('./services/appointmentService');
-const courseService = require('./services/courseService');
-const courseMembershipService = require('./services/courseMembershipService');
+// const courseMembershipService = require('./services/courseMembershipService');
 
 app.use(express.json());
 app.use(cors());
@@ -13,8 +14,7 @@ app.use(cors());
 // Define specific base routes for each service
 app.use('/schedules', scheduleService);
 app.use('/appointments', appointmentService);
-app.use('/courses', courseService);
-app.use('/course-memberships', courseMembershipService);
+// app.use('/course-memberships', courseMembershipService);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
